b6adffd396fbbca09fb222be1a32197f8dd3a737
enejH04 <hudobreznik.enej.04@gmail.com>
2026-08-16 19:58:32 +0200

    Add links for PyPI release

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "double-boot"
 copyright = "2026, Enej Hudobreznik"
 author = "Enej Hudobreznik"
-release = "1.0.2"
+release = "1.0.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
